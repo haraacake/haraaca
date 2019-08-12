b6583ffc4e3e16aeaf8bc7d5ca14d721e892a6ef
Mwamodo Erick <mwamodojnr@gmail.com>
2019-08-13 01:14:43 +0300

    pages

---
+++ pages/book-a-car.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <p>
+          Book A Car
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Home;
---
+++ pages/list-your-car.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <p>
+          List Your Car
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Home;
