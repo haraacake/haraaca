9233d43d3c551606fab3641256db6e39f34adf2a
Mwamodo Erick <mwamodojnr@gmail.com>
2019-08-17 09:33:29 +0300

    TownCard Improvement

--- components/TownCard.js
+++ components/TownCard.js
@@ -1,26 +1,29 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Link from 'next/link';
 
 const TownCard = props => {
   const { details } = props;
   return (
-    <div className="antialiased flex shadow-md mt-6 rounded md:m-2 lg:m-3">
-      <div>
-        <img
-          src={details.image}
-          alt={details.title}
-          className="rounded-l object-cover"
-        />
-      </div>
-      <div className="ml-3 md:mr-3 lg:mx-6 xl:mx-12 mt-2">
-        <p className="font-semibold uppercase tracking-wide text-lg">
-          {details.title}
-        </p>
-        <p className="p-0 uppercase tracking-wide text-sm text-gray-600 font-semibold">
-          {details.price} Cars
-        </p>
-      </div>
-    </div>
+    <Link href="/town">
+      <a className="antialiased flex shadow-md mt-6 rounded md:m-2 lg:m-3">
+        <div>
+          <img
+            src={details.image}
+            alt={details.title}
+            className="rounded-l object-cover"
+          />
+        </div>
+        <div className="ml-3 md:mr-3 lg:mx-6 xl:mx-12 mt-2">
+          <p className="font-semibold uppercase tracking-wide text-lg">
+            {details.title}
+          </p>
+          <p className="p-0 uppercase tracking-wide text-sm text-gray-600 font-semibold">
+            {details.price} Cars
+          </p>
+        </div>
+      </a>
+    </Link>
   );
 };
 
