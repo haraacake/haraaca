12bad89c685225131f8b8921c41a411070fcb505
Mwamodo Erick <mwamodojnr@gmail.com>
2019-08-19 09:18:43 +0300

    Messages page

--- pages/messages.js
+++ pages/messages.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 class Messages extends Component {
   state = {
@@ -17,6 +18,18 @@ class Messages extends Component {
       <>
         <div>
           <Nav auth />
+          <div className="h-48 bg-gray-900">{/* profile pic */}</div>
+          <div className="w-11/12 md:w-5/6 mx-auto">
+            <h1 className="text-4xl mt-10 font-bold tracking-wide">Messages</h1>
+          </div>
+          <div className="text-center mt-12">
+            <h2 className="text-gray-700 font-bold tracking-wide text-4xl">
+              No Messages yet
+            </h2>
+          </div>
+          <div className="mt-48">
+            <Footer />
+          </div>
         </div>
       </>
     );
